584f830fb55154d914883a4dc3491d17a7849a40
Karthik Shaji <58968966+HoodedPitohui@users.noreply.github.com>
2022-08-12 19:19:43 -0700

    Set up Buttons using uifigure

--- track-map/TrackMapperGUI.m
+++ track-map/TrackMapperGUI.m
@@ -1,35 +1,19 @@
-function [] = TrackMapperGUI()
-% Demonstrate how to use a pushbutton to delete bits of string and how to 
-% let the user know that their actions are futile.  After the string is 
-% deleted completely, the user is informed that there is nothing left to 
-% delete if the delete button is pressed again.  A color change accompanies
-% this announcement.
-%
-% Suggested exercise:  Add a counter to S that starts incrementing when the  
-% warning is given.  If the user clicks again, close the GUI.
-%
-%
-% Author:  Matt Fig
-% Date:  7/15/2009
+function TrackMapperGUI
 clc;
 clear all;
 close all;
 screenSize = get(groot, 'ScreenSize');
 buttonSize = [260 80];
+fillSize = [130 40];
+button1Pos = [screenSize(3) / 4 - buttonSize(1) / 4, 20, buttonSize(1), buttonSize(2)];
 
-S.fh = figure('units','pixels',...
-              'menubar','none',...
-              'name','Track Mapper',...
-              'numbertitle','off', 'WindowState', 'maximized');
-S.pb = uicontrol('style','push',...
-                 'unit','pix',...
-                 'position',[screenSize(3) / 2 - buttonSize(1) / 2, 20, 260, 80],...
-                 'string','Upload Image');
-set(S.pb,'callback',{@pb_call,S})  % Set the callback for pushbutton.
-function [] = mapTrack(im)
-    
-function [] = pb_call(varargin)
-    subplot(2, 1, 1);
+figInterface = uifigure('WindowState', 'maximized');
+figAxes = uiaxes(figInterface);
+uplImg = uibutton(figInterface, 'Text', 'Upload Image', 'Position', button1Pos, ...
+                   'ButtonPushedFcn', @(btn, event) uplButtonPushed(btn, figAxes));
+end
+
+function uplButtonPushed(btn, uiFigAxes)
     [File_Name, Path_Name] = uigetfile('D:\Users\Public\Pictures\Sample Pictures');
-    imshow([Path_Name,File_Name]);
-    
+    imshow([Path_Name,File_Name], "Parent", uiFigAxes);
+end
