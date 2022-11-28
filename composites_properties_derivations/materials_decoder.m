64a2979b1836a78a7a9b548f2b695d769eb6827f
Karthik Shaji <kshaji3@gatech.edu>
2022-11-28 16:55:19 -0500

    Basis done

--- composites_properties_derivations/materials_decoder.m
+++ composites_properties_derivations/materials_decoder.m
@@ -65,6 +65,18 @@ comp_tube_8.property_table = find_material_props(comp_tube_8, file_names(7));
 comp_tube_nb.property_table = find_material_props(comp_tube_nb, file_names(8));
 comp_tube_wb.property_table = find_material_props(comp_tube_wb, file_names(9));
 
+excel_file = "property_table.xlsx";
+writetable(comp_tube_2.property_table, excel_file, 'Sheet', 1);
+writetable(comp_tube_3.property_table, excel_file, 'Sheet', 2);
+writetable(comp_tube_4.property_table, excel_file, 'Sheet', 3);
+writetable(comp_tube_5.property_table, excel_file, 'Sheet', 4);
+writetable(comp_tube_6.property_table, excel_file, 'Sheet', 5);
+writetable(comp_tube_7.property_table, excel_file, 'Sheet', 6);
+writetable(comp_tube_8.property_table, excel_file, 'Sheet', 7);
+writetable(comp_tube_nb.property_table, excel_file, 'Sheet',8);
+writetable(comp_tube_wb.property_table, excel_file, 'Sheet', 9);
+
+
 test = 0;
 
 
@@ -83,31 +95,43 @@ function [property_table] = find_material_props(tube_struct, tube_name)
     %Data Value: NU12 = Poisson's ratio
     NU12 = 1;
     
-    %Data Value: G12 = Inplane Shear Modulus
-    arbit_force_1 = tube_struct.raw_data.Force(21);
-    arbit_deltax_1 = tube_struct.raw_data.EpsilonONEDisplacement(21);
+
     length = tube_struct.part_descript.Var1("Length") * 10^(-3);
     outer_diameter = tube_struct.part_descript.Var1("Outer diameter") * 10^(-3);
     wall_thickness = tube_struct.part_descript.Var1("Wall thickness") * 10^(-3);
     area = (outer_diameter^2 / 4 * pi) - (outer_diameter / 2 - wall_thickness)^2 ...
         * pi;
-    G12 = arbit_force_1 * 10^3 * length / (area * arbit_deltax_1);
-    
-    %Data Value: G1Z = Transverse Shear Modulus for Shear in 1-Z Plane
-    G1Z = 0;
-    
-    %Data Value: G2Z = Transverse Shear Modulus for Shear in 2-Z Plane
-    G2Z = 0;
-    
-    %Data Value: RHO = Mass Density
     volume = area * length;
     RHO = tube_struct.mass / volume;
     
+    %Data Value: G12 = Inplane Shear Modulus
+    arbit_force_1 = tube_struct.raw_data.Force(41);
+    arbit_deltax_1 = tube_struct.raw_data.EpsilonONEDisplacement(41);
+    G12 = arbit_force_1 * 10^3 * length / (area * arbit_deltax_1);
+    
+    %Data Value: G1Z = Transverse Shear Modulus for Shear in 1-Z Plane
+    I = (1 / 12) * RHO * volume * (3 * ((outer_diameter / 2)^2 + ...
+        (outer_diameter / 2 - wall_thickness)^2) + length^2);
+    G1Z = G12;
+    
+    %Data Value: G2Z = Transverse Shear Modulus for Shear in 2-Z Plane
+    G2Z = G1Z; %as cylinder
+    
+    %Data Value: RHO = Mass Density
+    
+    %According to: 
+    %https://cfmetrologie.edpsciences.org/articles/metrology/pdf/2019/01/metrology_cim2019_26003.pdf
+    %Thermal Expansion Coefficients are close to 0 for carbon fiber parts
+    %This paper: https://opg.optica.org/DirectPDFAccess/928A9EAB-5D70-40FE-953289C90098A3C1_380577/oe-26-1-531.pdf?da=1&id=380577&seq=0&mobile=no
+    %Found it to be on the order of 10^(-6), so it's negligible
+    %I'm using this:
+    %http://www.performance-composites.com/carbonfibre/mechanicalproperties_2.asp
+    %so 2.15 *10^(-6)
     %Data Value: A1 = Thermal expansion coefficient in 1-direction
-    A1 = "[Leave Blank]";
+    A1 = 2.15E-6;
     
     %Data Value: A2 = Thermal expansion coefficient in 2-direction
-    A2 = "[Leave Blank]";
+    A2 = 2.15E-6;
     
     %Data Value: TREF = Reference temperature for the calculation of
     %thermal loads
@@ -115,11 +139,15 @@ function [property_table] = find_material_props(tube_struct, tube_name)
     
     %Data Value: Xt, Xc, Yt, Yc: Allowable stresses or strains in the
     %longitudinal and lateral directions
-    XtXcYtYc = [0, 0, 0, 0];
+    max_stress = tube_struct.summary_stats.Var1("Tensile stress at Break (Standard)");
+    XtXcYtYc = [max_stress, max_stress, max_stress, max_stress];
     
     %Data Value: S = Allowable for in-plane shear stresses or strains for
     %composite ply failure calculations
-    S = 0;
+    %0.5 is apparently recommended for Tresca, 0.577 for Von-Misces
+    %criterion
+    %So we will use 0.5 as that's the lower bound (so more conservative)
+    S = 0.5 * max_stress;
     
     %Data Value: GE = Structural Element Damping Coefficient
     GE = "[Leave Blank]";
